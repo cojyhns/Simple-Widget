620374f48f421392839934e734ea93d57c4bdf6c
Vlad <cojyboi@gmail.com>
2025-11-21 16:58:55 +0200

    Initial Commit

---
+++ step counter/step_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  step_counterApp.swift
+//  step counter
+//
+//  Created by mac on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct step_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
